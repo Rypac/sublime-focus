5ee8520da657b6548a0b2e2681225cf22a23dbe7
Ryan Davis <ryan@rypac.org>
2023-04-17 22:07:22 +1000

    Apply focus mode settings to on_post_move event

--- focus.py
+++ focus.py
@@ -139,6 +139,10 @@ class FocusModeListener(sublime_plugin.EventListener):
         if (window := view.window()) and window.settings().has("focus_mode_state"):
             enter_view_focus_mode(view)
 
+    def on_post_move(self, view: sublime.View):
+        if (window := view.window()) and window.settings().has("focus_mode_state"):
+            enter_view_focus_mode(view)
+
     def on_load_project(self, window: sublime.Window):
         if window.settings().has("focus_mode_state"):
             apply_focus_mode_settings(window)
